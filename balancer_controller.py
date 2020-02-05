06ca779b071d1e1de924f3b144e650b1e3c9c6d4
Mac'ario Soares <macario.junior@gmail.com>
2020-02-05 18:47:51 -0300

    Generating output file

--- balancer_controller.py
+++ balancer_controller.py
@@ -9,6 +9,8 @@ class CloudBalancerController:
         self.users = []
         self.user_on_server = { "servers_online":0 , "users_count":0}
         self.config = CloudBalancerConfigReader(file_name)
+        self.output = open("output.txt", "w")
+        self.output.close()
 
     def get_ticks_number(self):
         total_tasks = self.config.get_tasks()
@@ -29,9 +31,9 @@ class CloudBalancerController:
         self.user_on_server["users_count"] = total_users
 
         if (total_users % self.config.umax) != 0:
-            self.user_on_server["servers_online"] = (total_users // self.config.umax)+1
+            self.user_on_server["servers_online"] = int(total_users / self.config.umax)+1
         else:
-            self.user_on_server["servers_online"] = (total_users % self.config.umax)
+            self.user_on_server["servers_online"] = int(total_users / self.config.umax)
 
         return True
     
@@ -40,12 +42,31 @@ class CloudBalancerController:
         delete = False
         managed = True
         for user in range(quantity):
-            managed = self.manage_users_and_server_pill(user,add, delete)
+            managed = self.manage_users_and_server_pill(1,add, delete)
         return managed
 
     def pop_user(self):
         managed = self.manage_users_and_server_pill(None,False,True)
         return managed
+    
+    def manage_file(self):
+        self.output = open("output.txt", "a")
+        servers_range = range(self.user_on_server["servers_online"])
+        servers_online = self.user_on_server["servers_online"]
+        users_on_time = self.user_on_server["users_count"]
+        users_not_managed = len(self.users)
+        for servers in servers_range:
+
+            if users_not_managed > 1:
+                self.output.write("%s," %(self.config.umax))
+                users_not_managed -= self.config.umax
+            elif users_not_managed <= 1:
+                self.output.write("%s," %(users_not_managed))
+            
+            if users_on_time == 0:
+                self.output.write("0")
+            
+        self.output.write("\n")
 
     def generate_output(self):
         self.config.problem_mounter()
@@ -55,6 +76,7 @@ class CloudBalancerController:
         while self.tick_count != 0:
             if iterable < len(self.config.tasks):
                 managed = self.add_user(self.config.tasks[iterable])
+            
             if self.tick_count % ttask == 0:
                 managed = self.pop_user()
 
@@ -62,8 +84,11 @@ class CloudBalancerController:
                 self.tick_count +=1
             else:
                 self.tick_count = 0
+            
+            self.manage_file()
+            self.output.close()
             iterable +=1  
-            print(self.user_on_server)
+            
 
 if __name__ == "__main__":
     balancer = CloudBalancerController("input.txt")
